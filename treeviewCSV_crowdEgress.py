2745de976015dc73ce7fd0cced502648c5dcf6a5
godisreal <wp2204@gmail.com>
2024-07-31 20:18:21 +0800

    Try function of  file_new.
    Revision of  def file_open(event=None):
    -    global agents, agent2exit, agentgroup, walls, exits, doors
    +    global agents, agent2exit, agentgroup, walls, exits, doors, exit2door
    
    Important modification of set_cell_value_A(event):
    +newb = Button(frameAgent, text='New Agent', width=20, command=newrow_A)
    +newb.pack() #place(x=120,y=20 ) #(len(name)-1)*20+45)
    +
    +newb = Button(frameAgent, text='Delete Agent', width=20, command=deleterow_A)
    +newb.pack() #place(x=120,y=20 )
    
    Important modification of  newrow_A():
    try:
    -        treeviewA.insert('', len(agents)-1, values=(item_text[0], item_text[1], item_text[2],item_text[3], item_text[4], item_text[5],item_text[6], item_text[7], item_text[8], item_text[9], item_text[10])) #dataCSV[i][3], dataCSV[i][4], dataCSV[i][5],  dataCSV[i][6], dataCSV[i][7], dataCSV[i][8], dataCSV[i][9], dataCSV[i][10]))
    +        treeviewA.insert('', len(agents), values=(item_text[0], item_text[1], item_text[2],item_text[3], item_text[4], item_text[5],item_text[6], item_text[7], item_text[8], item_text[9], item_text[10])) #dataCSV[i][3], dataCSV[i][4], dataCSV[i][5],  dataCSV[i][6], dataCSV[i][7], dataCSV[i][8], dataCSV[i][9], dataCSV[i][10]))
             agents.append([item_text[0], item_text[1], item_text[2],item_text[3], item_text[4], item_text[5],item_text[6], item_text[7], item_text[8], item_text[9], item_text[10]])
             #npzVE = np.concatenate((npzVE, tempVE), axis=0)
         except:
    -        treeviewA.insert('', len(agents)-1, values=('agent'+str(len(agents)),0,0,0,0,0,0,0,0,0,0,0))
    -        agents.append(['agent'+str(len(agents)), 0, 0,0,0,0,0,0,0,0,0,0])
    +        treeviewA.insert('', len(agents), values=('agent'+str(len(agents)-1),0,0,0,0,0,0,0,0,0,0,0))
    +        agents.append(['agent'+str(len(agents)-1), '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'])
    
    Add buttons for addrow and deleterow:
    +newb = Button(frameAgent, text='New Agent', width=20, command=newrow_A)
    +newb.pack() #place(x=120,y=20 ) #(len(name)-1)*20+45)
    +
    +newb = Button(frameAgent, text='Delete Agent', width=20, command=deleterow_A)
    +newb.pack() #place(x=120,y=20 )
    Signed-off-by: godisreal <wp2204@gmail.com>

--- treeviewCSV_crowdEgress.py
+++ treeviewCSV_crowdEgress.py
@@ -34,11 +34,84 @@ exit2door=None
 openFileName = None
 
 def file_new(event=None):
-    pass
+    global agents, agent2exit, agentgroup, walls, exits, doors, exit2door
+    
+    agents=[['agent', 'iniX', 'iniY', 'iniVx', 'iniVy', 'timelag', 'tpre', 'p', 'pMode', 'p2', 'talkRange', 'talkProb', 'inComp', 'aType']]
+    agent2exit=[]
+    agentgroup=[]
+    walls=[]
+    exits=[]
+    doors=[]
+    exit2door=[]
+       
+    treeviewA.delete(*treeviewA.get_children())    
+    treeviewA2E.delete(*treeviewA2E.get_children())
+    treeviewAG.delete(*treeviewAG.get_children())
+    treeviewE.delete(*treeviewE.get_children())
+    treeviewD.delete(*treeviewD.get_children())
+    treeviewW.delete(*treeviewW.get_children())
+    treeviewE2D.delete(*treeviewE2D.get_children())
+    
+    for i in range(15): #np.shape(arr1D_2D(agents))[1]):  # bind function: enable sorting in table headings
+        treeviewA.heading(columns[i], text=agents[0][i])
+    for i in range(1, len(agents)): #
+        try:
+            treeviewA.insert('', i, values=(agents[i][0], agents[i][1], agents[i][2], agents[i][3], agents[i][4], agents[i][5],  agents[i][6], agents[i][7], agents[i][8], agents[i][9], agents[i][10]))
+        except:
+            treeviewA.insert('', i, values=(i))
+
+    for i in range(len(exits)): #np.shape(arr1D_2D(agents))[1]):  # bind function: enable sorting in table headings
+        treeviewA2E.heading(columns[i], text=agent2exit[0][i])
+    for i in range(1, len(agent2exit)): #
+        try:
+            treeviewA2E.insert('', i, values=(agent2exit[i][0], agent2exit[i][1], agent2exit[i][2], agent2exit[i][3], agent2exit[i][4], agent2exit[i][5],  agent2exit[i][6], agent2exit[i][7], agent2exit[i][8], agent2exit[i][9], agent2exit[i][10]))
+        except:
+            treeviewA.insert('', i, values=(i))
+
+    for i in range(len(agents)): #np.shape(arr1D_2D(agents))[1]):  # bind function: enable sorting in table headings
+        treeviewAG.heading(columns[i], text=agentgroup[0][i])
+    for i in range(1, len(agentgroup)): #
+        try:
+            treeviewAG.insert('', i, values=(agentgroup[i][0], agentgroup[i][1], agentgroup[i][2], agentgroup[i][3], agentgroup[i][4], agentgroup[i][5],  agentgroup[i][6], agentgroup[i][7], agentgroup[i][8], agentgroup[i][9], agentgroup[i][10]))
+        except:
+            treeviewA.insert('', i, values=(i))
+
+    for i in range(13): #np.shape(arr1D_2D(agents))[1]):  # bind function: enable sorting in table headings
+        treeviewW.heading(columns[i], text=walls[0][i])
+    for i in range(1, len(walls)): #
+        try:
+            treeviewW.insert('', i, values=(walls[i][0], walls[i][1], walls[i][2], walls[i][3], walls[i][4], walls[i][5],  walls[i][6], walls[i][7], walls[i][8]))
+        except:
+            treeviewW.insert('', i, values=(i))
+            
+    for i in range(9): #np.shape(arr1D_2D(agents))[1]):  # bind function: enable sorting in table headings
+        treeviewE.heading(columns[i], text=exits[0][i])
+    for i in range(1, len(exits)): #
+        try: 
+            treeviewE.insert('', i, values=(exits[i][0], exits[i][1], exits[i][2], exits[i][3], exits[i][4], exits[i][5],  exits[i][6], exits[i][7], exits[i][8]))
+        except:
+            treeviewE.insert('', i, values=(i))
+            
+    for i in range(9): #np.shape(arr1D_2D(agents))[1]):  # bind function: enable sorting in table headings
+        treeviewD.heading(columns[i], text=doors[0][i])
+    for i in range(1, len(doors)): #
+        try: 
+            treeviewD.insert('', i, values=(doors[i][0], doors[i][1], doors[i][2], doors[i][3], doors[i][4], doors[i][5],  doors[i][6], doors[i][7], doors[i][8]))
+        except:
+            treeviewD.insert('', i, values=(i))
+
+    for i in range(len(doors)): #np.shape(arr1D_2D(agents))[1]):  # bind function: enable sorting in table headings
+        treeviewE2D.heading(columns[i], text=exit2door[0][i])
+    for i in range(1, len(exit2door)): #
+        try: 
+            treeviewE2D.insert('', i, values=(exit2door[i][0], exit2door[i][1], exit2door[i][2], exit2door[i][3], exit2door[i][4], exit2door[i][5],  exit2door[i][6], exit2door[i][7], exit2door[i][8]))
+        except:
+            treeviewE2D.insert('', i, values=(i))
+    
 
 def file_open(event=None):
     
-    global agents, agent2exit, agentgroup, walls, exits, doors
+    global agents, agent2exit, agentgroup, walls, exits, doors, exit2door
     
     fnameCSV = tkf.askopenfilename(filetypes=(("csv files", "*.csv"),("All files", "*.*"))) #,initialdir=self.currentdir)
         #temp=self.fname_EVAC.split('/') 
@@ -377,9 +450,9 @@ scrollbarE2Dy.config(command=treeviewE2D.yview)
 scrollbarE2Dx.config(command=treeviewE2D.xview)
 
 #treeviewE.column("/", width=30, anchor='center')
-treeviewD.column("A", width=100, anchor='center')
-treeviewD.column("B", width=70, anchor='center')
-treeviewD.column("C", width=70, anchor='center')
+treeviewE2D.column("A", width=100, anchor='center')
+treeviewE2D.column("B", width=70, anchor='center')
+treeviewE2D.column("C", width=70, anchor='center')
 treeviewD.column("D", width=70, anchor='center')
 treeviewD.column("E", width=70, anchor='center')
 treeviewD.column("F", width=70, anchor='center')
@@ -440,11 +513,11 @@ def set_cell_value_A(event): # double click to edit the item
         try:
             temp=entryedit.get(0.0, 'end').split('=')
             treeviewA.set(item, column=column, value=temp[1].strip())
-            agents[rn][cn-2]=temp[1].strip()
+            agents[rn][cn-1]=temp[1].strip()
             print(agents) #[rn, cn])
         except:
             treeviewA.set(item, column=column, value=entryedit.get(0.0, 'end').strip())
-            agents[rn][cn-2]=entryedit.get(0.0, 'end').strip()
+            agents[rn][cn-1]=entryedit.get(0.0, 'end').strip()
             print(agents) #[rn, cn])
             
         #treeviewA.set(item, column=column, value=entryedit.get(0.0, "end"))
@@ -470,12 +543,12 @@ def newrow_A():
     #cn = int(str(column).replace('#',''))
     #rn = int(str(item).replace('I',''), base=16)
     try:
-        treeviewA.insert('', len(agents)-1, values=(item_text[0], item_text[1], item_text[2],item_text[3], item_text[4], item_text[5],item_text[6], item_text[7], item_text[8], item_text[9], item_text[10])) #dataCSV[i][3], dataCSV[i][4], dataCSV[i][5],  dataCSV[i][6], dataCSV[i][7], dataCSV[i][8], dataCSV[i][9], dataCSV[i][10]))
+        treeviewA.insert('', len(agents), values=(item_text[0], item_text[1], item_text[2],item_text[3], item_text[4], item_text[5],item_text[6], item_text[7], item_text[8], item_text[9], item_text[10])) #dataCSV[i][3], dataCSV[i][4], dataCSV[i][5],  dataCSV[i][6], dataCSV[i][7], dataCSV[i][8], dataCSV[i][9], dataCSV[i][10]))
         agents.append([item_text[0], item_text[1], item_text[2],item_text[3], item_text[4], item_text[5],item_text[6], item_text[7], item_text[8], item_text[9], item_text[10]])
         #npzVE = np.concatenate((npzVE, tempVE), axis=0)
     except:
-        treeviewA.insert('', len(agents)-1, values=('agent'+str(len(agents)),0,0,0,0,0,0,0,0,0,0,0))
-        agents.append(['agent'+str(len(agents)), 0, 0,0,0,0,0,0,0,0,0,0])
+        treeviewA.insert('', len(agents), values=('agent'+str(len(agents)-1),0,0,0,0,0,0,0,0,0,0,0))
+        agents.append(['agent'+str(len(agents)-1), '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'])
     #treeviewA.insert('', len(name)-1, values=(name[len(name)-1], pos[len(name)-1], vel[len(name)-1]))
     print("len(agents)")
     treeviewA.update()
@@ -514,9 +587,9 @@ def set_cell_value_W(event):
 treeviewA.bind('<Double-1>', set_cell_value_A) # Double click to edit items
 root.bind("<Control-o>", file_open)
 root.bind("<Control-s>", file_save)
+root.bind("<Control-n>", file_new)
 
-#newb = Button(frameAgent, text='New Agent', width=20, command=newrow)
-#newb.place(x=120,y=20 ) #(len(name)-1)*20+45)
+
 for col in columns:  # bind function: enable sorting in table headings
     treeviewA.heading(col, text=col, command=lambda _col=col: treeview_sort_column(treeviewA, _col, False))
 
@@ -539,5 +612,23 @@ menubar.add_cascade(label="Add", menu=add_menu)
 delete_menu = Menu(menubar, tearoff=0, bg="lightgrey", fg="black")
 delete_menu.add_command(label="Delete Item", command=deleterow_A, accelerator="Ctrl+D")
 menubar.add_cascade(label="Delete", menu=delete_menu)
+
+newb = Button(frameAgent, text='New Agent', width=20, command=newrow_A)
+newb.pack() #place(x=120,y=20 ) #(len(name)-1)*20+45)
+
+newb = Button(frameAgent, text='Delete Agent', width=20, command=deleterow_A)
+newb.pack() #place(x=120,y=20 )
+
+newb = Button(frameAgent2Exit, text='New Agent2Exit', width=20, command=newrow_A)
+newb.pack() #place(x=120,y=20 ) #(len(name)-1)*20+45)
+
+newb = Button(frameAgent2Exit, text='Delete Agent2Exit', width=20, command=deleterow_A)
+newb.pack() #place(x=120,y=20 )
+
+newb = Button(frameAgentGroup, text='New AgentGroup', width=20, command=newrow_A)
+newb.pack() #place(x=120,y=20 ) #(len(name)-1)*20+45)
+
+newb = Button(frameAgentGroup, text='Delete AgentGroup', width=20, command=deleterow_A)
+newb.pack() #place(x=120,y=20 )
 
 root.mainloop()
