abef98f7f180781cca525b555b21e2cc18c062d0
godisreal <wp2204@gmail.com>
2024-08-27 21:00:22 +0800

    Modify textCSV_Edit.py for both python3 and python2.7. Modify file_save() and file_open()
         def file_save(self, event=None):
             if not self.open_file:
    -            new_file_name = filedialog.asksaveasfilename()
    +            new_file_name = tkf.asksaveasfilename()
                 if new_file_name:
                     self.open_file = new_file_name
    -            if self.open_file:
    -                new_contents = self.main_text.get(1.0, tk.END)
    -                with open(self.open_file, "w") as open_file:
    -                    open_file.write(new_contents)
    +        if self.open_file:
    +            #new_contents = self.main_text.get(1.0, END)
    +            #with open(self.open_file, "w") as open_file:
    +            #    open_file.write(new_contents)
    +            new_contents = self.main_text.get(1.0, END)
    +            new_contents2 = re.sub(',\t', ',', new_contents)
    +            with open(self.open_file, "w") as open_file:
    +                open_file.write(new_contents2)
    
         def file_open(self, event=None):
    -        file_to_open = filedialog.askopenfilename()
    +        file_to_open = tkf.askopenfilename()
    
             if file_to_open:
                 self.open_file = file_to_open
    -            self.main_text.delete(1.0, tk.END)
    +            self.main_text.delete(1.0, END)
    
                 with open(file_to_open, "r") as file_contents:
                     file_lines = file_contents.readlines()
                     if len(file_lines) > 0:
                         for index, line in enumerate(file_lines):
                             index = float(index) + 1.0
    +                        line = re.sub(r',,', '', line)
                             line_t = re.sub(r',', ',\t', line)
                             self.main_text.insert(index, line_t)
                             #self.main_text.insert(index, line)
    -        self.title(" - ".join([self.WINDOW_TITLE, self.open_file]))
    +        self.window.title(" - ".join([self.WINDOW_TITLE, self.open_file]))
             #self.update_line_numbers()
    
         def file_save(self, event=None):
             if not self.open_file:
    -            new_file_name = filedialog.asksaveasfilename()
    +            new_file_name = tkf.asksaveasfilename()
                 if new_file_name:
                     self.open_file = new_file_name
    -            if self.open_file:
    -                new_contents = self.main_text.get(1.0, tk.END)
    -                with open(self.open_file, "w") as open_file:
    -                    open_file.write(new_contents)
    +        if self.open_file:
    +            #new_contents = self.main_text.get(1.0, END)
    +            #with open(self.open_file, "w") as open_file:
    +            #    open_file.write(new_contents)
    +            new_contents = self.main_text.get(1.0, END)
    +            new_contents2 = re.sub(',\t', ',', new_contents)
    +            with open(self.open_file, "w") as open_file:
    +                open_file.write(new_contents2)
    
    Signed-off-by: godisreal <wp2204@gmail.com>

--- textCSV_Edit.py
+++ textCSV_Edit.py
@@ -1,6 +1,7 @@
 
-import tkinter as tk
-from tkinter import filedialog
+
+#import tkinter as tk
+#from tkinter import filedialog
 from functools import partial
 import re, sys, os
 
@@ -12,7 +13,6 @@ if sys.version_info[0] == 3: # Python 3
     from tkinter.ttk import Treeview
     from tkinter.ttk import Button
     import tkinter.filedialog as tkf
-    import tkinter.messagebox as msg
 else:
     # Python 2
     from Tkinter import *
@@ -20,10 +20,9 @@ else:
     from ttk import Treeview
     from ttk import Entry
     import tkFileDialog as tkf
-    import tkMessageBox as msg
     
-
-class FindPopup(tk.Toplevel):
+'''
+class FindPopup(Toplevel):
     def __init__(self, master):
         super().__init__()
         self.master = master
@@ -33,21 +32,21 @@ class FindPopup(tk.Toplevel):
         self.transient(master)
         self.matches_are_highlighted = True
     
-        self.main_frame = tk.Frame(self, bg="lightgrey")
-        self.button_frame = tk.Frame(self.main_frame, bg="lightgrey")
-        self.find_label = tk.Label(self.main_frame, text="Find: ", bg="lightgrey", fg="black")
-        self.find_entry = tk.Entry(self.main_frame, bg="white", fg="black")
-        self.find_button = tk.Button(self.button_frame, text="Find All", bg="lightgrey", fg="black", command=self.find)
-        self.next_button = tk.Button(self.button_frame, text="Next", bg="lightgrey", fg="black", command=self.jump_to_next_match)
-        self.cancel_button = tk.Button(self.button_frame, text="Cancel", bg="lightgrey", fg="black", command=self.cancel)
+        self.main_frame = Frame(self, bg="lightgrey")
+        self.button_frame = Frame(self.main_frame, bg="lightgrey")
+        self.find_label = Label(self.main_frame, text="Find: ", bg="lightgrey", fg="black")
+        self.find_entry = Entry(self.main_frame, bg="white", fg="black")
+        self.find_button = Button(self.button_frame, text="Find All", bg="lightgrey", fg="black", command=self.find)
+        self.next_button = Button(self.button_frame, text="Next", bg="lightgrey", fg="black", command=self.jump_to_next_match)
+        self.cancel_button = Button(self.button_frame, text="Cancel", bg="lightgrey", fg="black", command=self.cancel)
     
-        self.main_frame.pack(fill=tk.BOTH, expand=1)
-        self.find_button.pack(side=tk.LEFT, pady=(0,10), padx=(20,20))
-        self.next_button.pack(side=tk.LEFT, pady=(0,10), padx=(15,20))
-        self.cancel_button.pack(side=tk.LEFT, pady=(0,10), padx=(15,0))
-        self.button_frame.pack(side=tk.BOTTOM, fill=tk.BOTH)
-        self.find_label.pack(side=tk.LEFT, fill=tk.X, padx=(20,0))
-        self.find_entry.pack(side=tk.LEFT, fill=tk.X, expand=1, padx=(0,20))
+        self.main_frame.pack(fill=BOTH, expand=1)
+        self.find_button.pack(side=LEFT, pady=(0,10), padx=(20,20))
+        self.next_button.pack(side=LEFT, pady=(0,10), padx=(15,20))
+        self.cancel_button.pack(side=LEFT, pady=(0,10), padx=(15,0))
+        self.button_frame.pack(side=BOTTOM, fill=BOTH)
+        self.find_label.pack(side=LEFT, fill=X, padx=(20,0))
+        self.find_entry.pack(side=LEFT, fill=X, expand=1, padx=(0,20))
     
         self.find_entry.focus_force()
         self.find_entry.bind("<Return>", self.jump_to_next_match)
@@ -95,26 +94,29 @@ class FindPopup(tk.Toplevel):
     
         geometry = "{}x{}+{}+{}".format(my_width, my_height, pos_x, pos_y)
         self.geometry(geometry)
-        
+'''
 
-class Editor(tk.Tk):
+class Editor(object):
     def __init__(self):
-        super().__init__()
     
         self.FONT_SIZE = 10
         #self.AUTOCOMPLETE_WORDS = ["def", "import", "if", "else", "while", "for","try:", "except:", "print(", "True", "False"]
         self.WINDOW_TITLE = "CSV Text Editor"
         self.open_file = ""
-        self.title(self.WINDOW_TITLE)
-        self.geometry("800x600")
-        self.menubar = tk.Menu(self, bg="lightgrey", fg="black")
+
+        self.window = Tk()
+        self.window.title(self.WINDOW_TITLE)
+        self.window.geometry("800x600")
+
+        self.menubar = Menu(self.window, bg="lightgrey", fg="black")
+        self.window.config(menu=self.menubar)
     
-        self.file_menu = tk.Menu(self.menubar, tearoff=0, bg="lightgrey", fg="black")
+        self.file_menu = Menu(self.menubar, tearoff=0, bg="lightgrey", fg="black")
         self.file_menu.add_command(label="New", command=self.file_new, accelerator="Ctrl+N")
         self.file_menu.add_command(label="Open", command=self.file_open, accelerator="Ctrl+O")
         self.file_menu.add_command(label="Save", command=self.file_save, accelerator="Ctrl+S")
         
-        self.edit_menu = tk.Menu(self.menubar, tearoff=0, bg="lightgrey", fg="black")
+        self.edit_menu = Menu(self.menubar, tearoff=0, bg="lightgrey", fg="black")
         self.edit_menu.add_command(label="Cut", command=self.edit_cut, accelerator="Ctrl+X")
         self.edit_menu.add_command(label="Paste", command=self.edit_paste, accelerator="Ctrl+V")
         self.edit_menu.add_command(label="Undo", command=self.edit_undo, accelerator="Ctrl+Z")
@@ -122,35 +124,32 @@ class Editor(tk.Tk):
     
         self.menubar.add_cascade(label="File", menu=self.file_menu)
         self.menubar.add_cascade(label="Edit", menu=self.edit_menu)
-        self.configure(menu=self.menubar)
+
         
         
         #self.line_numbers = Text(self, bg="lightgrey", fg="black", width=6, font=("Times", self.FONT_SIZE))
         #self.line_numbers.insert(1.0, "1 \n")
         #self.line_numbers.configure(state="disabled")
-        #self.line_numbers.pack(side=tk.LEFT, fill=tk.Y)
+        #self.line_numbers.pack(side=LEFT, fill=Y)
                 
-        self.main_text = tk.Text(self, bg="white", fg="black", font=("Times", self.FONT_SIZE))
-        self.main_text.pack(expand=1, fill=tk.BOTH)
+        self.main_text = Text(self.window, bg="white", fg="black", font=("Times", self.FONT_SIZE))
+        self.main_text.pack(expand=1, fill=BOTH)
 
         #self.scrollbar = Scrollbar(self, orient="vertical") #, command=self.scroll_text_and_line_numbers)
         #self.main_text.configure(yscrollcommand=self.scrollbar.set)
         #self.scrollbar.config(command=self.main_text.yview)
-        #self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        #self.scrollbar.pack(side=RIGHT, fill=Y)
     
         self.main_text.bind("<space>", self.destroy_autocomplete_menu)
         #self.main_text.bind("<KeyRelease>", self.display_autocomplete_menu)
         self.main_text.bind("<Tab>", self.insert_spaces)
     
-        self.bind("<Control-s>", self.file_save)
-        self.bind("<Control-o>", self.file_open)
-        self.bind("<Control-n>", self.file_new)
+        self.window.bind("<Control-s>", self.file_save)
+        self.window.bind("<Control-o>", self.file_open)
+        self.window.bind("<Control-n>", self.file_new)
         
-        self.bind("<Control-a>", self.select_all)
-        self.bind("<Control-f>", self.show_find_window)
-
-        self.bind("<Control-z>", self.edit_undo)
-        self.bind("<Control-y>", self.edit_redo)
+        self.window.bind("<Control-a>", self.select_all)
+        self.window.bind("<Control-f>", self.show_find_window)
 
     '''
         self.main_text.bind("<MouseWheel>", self.scroll_text_and_line_numbers)
@@ -184,81 +183,87 @@ class Editor(tk.Tk):
     '''
         
     def file_new(self, event=None):
-        file_name = filedialog.asksaveasfilename()
+        file_name = tkf.asksaveasfilename()
         if file_name:
             self.open_file = file_name
-            self.main_text.delete(1.0, tk.END)
+            self.main_text.delete(1.0, END)
             self.title(" - ".join([self.WINDOW_TITLE, self.open_file]))
 
+
     def file_open(self, event=None):
-        file_to_open = filedialog.askopenfilename()
+        file_to_open = tkf.askopenfilename()
 
         if file_to_open:
             self.open_file = file_to_open
-            self.main_text.delete(1.0, tk.END)
+            self.main_text.delete(1.0, END)
 
             with open(file_to_open, "r") as file_contents:
                 file_lines = file_contents.readlines()
                 if len(file_lines) > 0:
                     for index, line in enumerate(file_lines):
                         index = float(index) + 1.0
+                        line = re.sub(r',,', '', line)
                         line_t = re.sub(r',', ',\t', line)
                         self.main_text.insert(index, line_t)
                         #self.main_text.insert(index, line)
-        self.title(" - ".join([self.WINDOW_TITLE, self.open_file]))
+        self.window.title(" - ".join([self.WINDOW_TITLE, self.open_file]))
         #self.update_line_numbers()
         
 
     def file_save(self, event=None):
         if not self.open_file:
-            new_file_name = filedialog.asksaveasfilename()
+            new_file_name = tkf.asksaveasfilename()
             if new_file_name:
                 self.open_file = new_file_name
-            if self.open_file:
-                new_contents = self.main_text.get(1.0, tk.END)
-                with open(self.open_file, "w") as open_file:
-                    open_file.write(new_contents)
+        if self.open_file:
+            #new_contents = self.main_text.get(1.0, END)
+            #with open(self.open_file, "w") as open_file:
+            #    open_file.write(new_contents)
+            new_contents = self.main_text.get(1.0, END)
+            new_contents2 = re.sub(',\t', ',', new_contents)
+            with open(self.open_file, "w") as open_file:
+                open_file.write(new_contents2)
 
     def select_all(self, event=None):
-        self.main_text.tag_add("sel", 1.0, tk.END)
+        self.main_text.tag_add("sel", 1.0, END)
         return None
 
     def edit_cut(self, event=None):
         self.main_text.event_generate("<<Cut>>")
-        return "break"
+        return None
 
     def edit_paste(self, event=None):
         self.main_text.event_generate("<<Paste>>")
         self.on_key_release()
         self.tag_all_lines()
-        return "break"
+        return None
 
     def edit_undo(self, event=None):
         self.main_text.event_generate("<<Undo>>")
-        return "break"
+        return None
 
     def edit_redo(self, event=None):
         self.main_text.event_generate("<<Redo>>")
-        return "break"
+        return None
 
     def insert_spaces(self, event=None):
-        self.main_text.insert(tk.INSERT, " ")
-        return "break"
+        self.main_text.insert(INSERT, " ")
+        return None
 
     def get_menu_coordinates(self):
-        bbox = self.main_text.bbox(tk.INSERT)
+        bbox = self.main_text.bbox(INSERT)
         menu_x = bbox[0] + self.winfo_x() + self.main_text.winfo_x()
         menu_y = bbox[1] + self.winfo_y() + self.main_text.winfo_y() + self.FONT_SIZE + 2
         
         return (menu_x, menu_y)
 
     def display_autocomplete_menu(self, event=None):
-        current_index = self.main_text.index(tk.INSERT)
+        current_index = self.main_text.index(INSERT)
         start = self.adjust_floating_index(current_index)
 
         try:
-            currently_typed_word = self.main_text.get(start + " wordstart", tk.INSERT)
-        except tk.TclError:
+            currently_typed_word = self.main_text.get(start + " wordstart", INSERT)
+        except TclError:
             currently_typed_word = ""
         
         currently_typed_word = str(currently_typed_word).strip()
@@ -273,7 +278,7 @@ class Editor(tk.Tk):
 
             if len(suggestions) > 0:
                 x, y = self.get_menu_coordinates()
-                self.complete_menu = tk.Menu(self, tearoff=0, bg="lightgrey", fg="black")
+                self.complete_menu = Menu(self, tearoff=0, bg="lightgrey", fg="black")
 
                 for word in suggestions:
                     insert_word_callback = partial(self.insert_word, word=word, part=
@@ -298,7 +303,7 @@ class Editor(tk.Tk):
         remaining_word = word[amount_typed:]
         remaining_word_offset = " +" + str(len(remaining_word)) + "c"
         self.main_text.insert(index, remaining_word)
-        self.main_text.mark_set(tk.INSERT, index + remaining_word_offset)
+        self.main_text.mark_set(INSERT, index + remaining_word_offset)
         self.destroy_autocomplete_menu()
         self.main_text.focus_force()
 
@@ -314,12 +319,12 @@ class Editor(tk.Tk):
         try:
             self.complete_menu.focus_force()
             self.complete_menu.entryconfig(0, state="active")
-        except tk.TclError:
+        except TclError:
             pass
     
     '''
     def tag_all_lines(self):
-        final_index = self.main_text.index(tk.END)
+        final_index = self.main_text.index(END)
         final_line_number = int(final_index.split(".")[0])
 
         for line_number in range(final_line_number):
@@ -329,8 +334,8 @@ class Editor(tk.Tk):
     
     def update_line_numbers(self):
         self.line_numbers.configure(state="normal")
-        self.line_numbers.delete(1.0, tk.END)
-        number_of_lines = self.main_text.index(tk.END).split(".")[0]
+        self.line_numbers.delete(1.0, END)
+        number_of_lines = self.main_text.index(END).split(".")[0]
         line_number_string = "\n".join(str(no+1) for no in range(int(number_of_lines)))
         self.line_numbers.insert(1.0, line_number_string)
         self.line_numbers.configure(state="disabled")
@@ -340,12 +345,12 @@ class Editor(tk.Tk):
         FindPopup(self)
     
     def highlight_matches(self, text_to_find):
-        self.main_text.tag_remove("findmatch", 1.0, tk.END)
+        self.main_text.tag_remove("findmatch", 1.0, END)
         self.match_coordinates = []
         self.current_match = -1
     
         find_regex = re.compile(text_to_find)
-        search_text_lines = self.main_text.get(1.0, tk.END).split("\n")
+        search_text_lines = self.main_text.get(1.0, END).split("\n")
     
         for line_number, line in enumerate(search_text_lines):
             line_number += 1
@@ -375,15 +380,17 @@ class Editor(tk.Tk):
                     self.current_match = -1
                     self.next_match()
                 else:
-                    self.main_text.mark_set(tk.INSERT, next_target)
+                    self.main_text.mark_set(INSERT, next_target)
                     self.main_text.tag_remove("findmatch", next_target, target_end)
                     self.main_text.tag_add("sel", next_target, target_end)
                     self.main_text.see(next_target)
     
     def remove_all_find_tags(self):
-        self.main_text.tag_remove("findmatch", 1.0, tk.END)
-        self.main_text.tag_remove("sel", 1.0, tk.END)
+        self.main_text.tag_remove("findmatch", 1.0, END)
+        self.main_text.tag_remove("self", 1.0, END)
+    def start(self):
+        self.window.mainloop()
 
 if __name__ == "__main__":
     editor = Editor()
-    editor.mainloop()
+    editor.start()
